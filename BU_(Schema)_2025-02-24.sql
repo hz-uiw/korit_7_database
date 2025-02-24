7a1a848dc329460714faa05420d54099f5481808
hz-uiw <kwonmc5.11@gmail.com>
2025-02-24 22:57:11 +0900

    스키마 백업

--- BU_(Schema)_2025-02-24.sql
+++ BU_(Schema)_2025-02-24.sql
@@ -1,126 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `board_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `board_project`;
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: board_project
--- ------------------------------------------------------
--- Server version	8.0.40
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `role_tb`
---
-
-DROP TABLE IF EXISTS `role_tb`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `role_tb` (
-  `role_id` int NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(255) NOT NULL,
-  `role_name_kor` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL,
-  PRIMARY KEY (`role_id`),
-  UNIQUE KEY `role_name_UNIQUE` (`role_name`),
-  UNIQUE KEY `role_name_kor_UNIQUE` (`role_name_kor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role_tb`
---
-
-LOCK TABLES `role_tb` WRITE;
-/*!40000 ALTER TABLE `role_tb` DISABLE KEYS */;
-/*!40000 ALTER TABLE `role_tb` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_role_tb`
---
-
-DROP TABLE IF EXISTS `user_role_tb`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_role_tb` (
-  `user_role_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `role_id` int NOT NULL,
-  `created_at` timestamp NOT NULL,
-  PRIMARY KEY (`user_role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_role_tb`
---
-
-LOCK TABLES `user_role_tb` WRITE;
-/*!40000 ALTER TABLE `user_role_tb` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_role_tb` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_tb`
---
-
-DROP TABLE IF EXISTS `user_tb`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_tb` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `nickname` varchar(255) NOT NULL,
-  `oauth2_name` varchar(255) DEFAULT NULL,
-  `oauth2_provider` varchar(255) DEFAULT NULL,
-  `account_expired` int NOT NULL,
-  `account_locked` int NOT NULL,
-  `credentials_expired` int NOT NULL,
-  `account_enabled` int NOT NULL,
-  `created_at` timestamp NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `email_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_tb`
---
-
-LOCK TABLES `user_tb` WRITE;
-/*!40000 ALTER TABLE `user_tb` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_tb` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping events for database 'board_project'
---
-
---
--- Dumping routines for database 'board_project'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-02-24 16:24:24
 CREATE DATABASE  IF NOT EXISTS `study` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `study`;
 -- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
@@ -1287,7 +1164,131 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-24 16:24:24
+-- Dump completed on 2025-02-24 22:56:45
+CREATE DATABASE  IF NOT EXISTS `board_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `board_project`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: board_project
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `role_tb`
+--
+
+DROP TABLE IF EXISTS `role_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role_tb` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  `role_name_kor` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `role_name_UNIQUE` (`role_name`),
+  UNIQUE KEY `role_name_kor_UNIQUE` (`role_name_kor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_tb`
+--
+
+LOCK TABLES `role_tb` WRITE;
+/*!40000 ALTER TABLE `role_tb` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role_tb`
+--
+
+DROP TABLE IF EXISTS `user_role_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_role_tb` (
+  `user_role_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `role_id` int NOT NULL,
+  `created_at` timestamp NOT NULL,
+  PRIMARY KEY (`user_role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role_tb`
+--
+
+LOCK TABLES `user_role_tb` WRITE;
+/*!40000 ALTER TABLE `user_role_tb` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_role_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_tb`
+--
+
+DROP TABLE IF EXISTS `user_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_tb` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `oauth2_name` varchar(255) DEFAULT NULL,
+  `oauth2_provider` varchar(255) DEFAULT NULL,
+  `account_expired` int NOT NULL,
+  `account_locked` int NOT NULL,
+  `credentials_expired` int NOT NULL,
+  `account_enabled` int NOT NULL,
+  `created_at` timestamp NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_tb`
+--
+
+LOCK TABLES `user_tb` WRITE;
+/*!40000 ALTER TABLE `user_tb` DISABLE KEYS */;
+INSERT INTO `user_tb` VALUES (1,'aaaa1','a2','a3','a4','a5','a6',1,1,1,1,'2025-02-24 08:12:56'),(2,'hzuiw','qwer1234!','hzuiw@gmail.com','hzuiw',NULL,NULL,1,1,1,1,'2025-02-24 13:53:49');
+/*!40000 ALTER TABLE `user_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'board_project'
+--
+
+--
+-- Dumping routines for database 'board_project'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-24 22:56:45
 CREATE DATABASE  IF NOT EXISTS `mini_post_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `mini_post_db`;
 -- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
@@ -1413,4 +1414,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-24 16:24:24
+-- Dump completed on 2025-02-24 22:56:45
